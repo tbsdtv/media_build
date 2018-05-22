22135571ac56f40e3f4887648626ad70375da196
CrazyCat <crazycat69@narod.ru>
2018-05-22 17:43:15 +0300

    Update source snapshot URLs.

--- archive.sh
+++ archive.sh
@@ -4,7 +4,7 @@ make cleanall
 make dir DIR=./media
 cd linux
 make todaytar DIR=.
-mv -f linux-media-LATEST.tar.* ..
+mv -f linux-media.tar.* ..
 
 echo "V4L firmwares archive..."
 cd ../v4l/firmware
