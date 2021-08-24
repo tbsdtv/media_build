378f61222ebde6a097541edc51712870a86fbe97
CrazyCat <crazycat69@narod.ru>
2021-08-24 17:46:23 +0300

    Fix install path and extend clean list.

--- install.sh
+++ install.sh
@@ -16,9 +16,8 @@ echo "V4L drivers installing..."
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/media
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/staging/media
 #sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/misc/altera-stapl
-sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/linux
-sudo rm -r -f /lib/modules/$(uname -r)/extra
-
+#sudo rm -r -f /lib/modules/$(uname -r)/kernel/drivers/linux
+sudo rm -r -f /lib/modules/$(uname -r)/updates/extra
 
 sudo make install
 
